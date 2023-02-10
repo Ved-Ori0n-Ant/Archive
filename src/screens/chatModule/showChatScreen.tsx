061a29ac2326f0815e4d2a4eeeb1e7af1afdccc4
Ved-Ori0n-Ant <190310116013@lecmorbi.gujgov.edu.in>
2023-02-10 18:29:56 +0530

    ui changes

--- src/screens/chatModule/showChatScreen.tsx
+++ src/screens/chatModule/showChatScreen.tsx
@@ -3,6 +3,8 @@ import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import { onValue, push, ref } from 'firebase/database';
 import { db } from '../../../firebase-config';
 import { useRoute } from '@react-navigation/native';
+import { useWindowDimensions, View } from 'react-native';
+import TextComponent from '../../customComponents/textComponent';
 const ShowChat = () => {
     
     const [messages, setMessages] = React.useState<any>([]);
@@ -19,18 +21,18 @@ const ShowChat = () => {
 
 
     React.useEffect(() => {
-        onValue(ref(db, '/chat/personalMessages'), (querySnapShot: any) => {
+        onValue(ref(db, '/chat/personalMessages/' + params?.fromUserData?.id + params?.item?.id + '/'), (querySnapShot: any) => {
             let temp: any = querySnapShot.val()
             let tempSpreader: any = {...temp}
             let tempIdStripped: any = Object.values(tempSpreader)
-            let val: any = tempIdStripped[0]
-            // setMessages([val])
-            console.log(val, ':::::::::............value............:::::::::')
+            // console.log('Line 28..',tempIdStripped, 'of user', params?.fromUserData[0]?.name)
+            setMessages([tempIdStripped])
         })
     }, [])
 
     const onSend = useCallback((messageArray: any[]) => {
         const myMsg = messageArray[0]
+        console.log('myMsg', myMsg)
         const msg = {
             ...myMsg, 
             recieverId: params?.item?.id, 
@@ -39,27 +41,48 @@ const ShowChat = () => {
         }
         console.log(msg)
         setMessages((previousMessages: any) => GiftedChat.append(previousMessages as any, messageArray))
-        push(ref(db, '/chat/personalMessages'), { msg })
+        push(ref(db, '/chat/personalMessages/' + params?.fromUserData?.id + params?.item?.id + '/'), { msg })
     }, [])
 
-    console.log(messages, ':::::Messages::::: of user ', params?.fromUserData[0].name)
+    // console.log(messages, ':::::Messages::::: of user ', params?.fromUserData[0].name)
     return (
+      <>
+        <View
+          style={{
+            backgroundColor: "#afcfcf",
+            width: "100%",
+            alignItems: "center",
+            height: 'auto',
+            maxHeight: 180,
+            justifyContent: "center",
+          }}
+        >
+          <TextComponent text={params?.fromUserData[0]?.name} />
+        </View>
         <GiftedChat
-            messages={messages}
-            onSend={messages => onSend(messages)}
-            user={{_id: params?.fromUserData[0]?.id}}
-            renderBubble={(props: any) => {
-                return (<Bubble {...props} wrapperStyle={{
-                    right: {
-                        backgroundColor: '#acacac',
-                    },
-                    left: {
-                        backgroundColor: '#0af',
-                    }
-                }} />)
-            }}
+          messages={messages}
+          onSend={(messages) => {onSend(messages); console.log( 'line 64', params?.fromUserData[0]?.id)}}
+          user={{ _id: params?.fromUserData[0]?.id }}
+          isLoadingEarlier
+          isTyping
+          renderBubble={(props: any) => {
+            return (
+              <Bubble
+                {...props}
+                // wrapperStyle={{
+                //   right: {
+                //     backgroundColor: "#acacac",
+                //   },
+                //   left: {
+                //     backgroundColor: "#0af",
+                //   },
+                // }}
+              />
+            );
+          }}
         />
-    )
+      </>
+    );
 }
 
 export default ShowChat
